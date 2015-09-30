2321b6099a2401bfcf7decd41842a9433a9ae219
EduAwaSeka <edu.rockero5@gmail.com>
2015-09-30 19:59:48 +0200

    Structure version 2

--- public_html/app/js/scripts.js
+++ public_html/app/js/scripts.js
@@ -1,7 +1,29 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
+$('#markdown-mode').on('click', function() {
+    $('#vertical-toolbar').removeClass('col-sm-2');
+    $('#vertical-toolbar').addClass('col-sm-6');
+
+    $('#workspace').removeClass('col-sm-10');
+    $('#workspace').addClass('col-sm-6');
+
+    $('#horizontal-slides').removeClass('col-sm-12');
+    $('#horizontal-slides').addClass('col-sm-3');
 
 
+    $('#markdown-space').removeClass('hidden');
+
+});
+
+
+$('#normal-mode').on('click', function() {
+    $('#vertical-toolbar').removeClass('col-sm-6');
+    $('#vertical-toolbar').addClass('col-sm-2');
+
+    $('#workspace').removeClass('col-sm-6');
+    $('#workspace').addClass('col-sm-10');
+
+    $('#horizontal-slides').removeClass('col-sm-3');
+    $('#horizontal-slides').addClass('col-sm-12');
+
+    $('#markdown-space').addClass('hidden');
+});
